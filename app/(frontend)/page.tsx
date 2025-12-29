f5d6d0207cd09513b06a017ff91ed1f084362d8d
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-29 23:00:35 +0200

    Update Home component to include About section and implement error handling for data fetching, improving user experience and content structure.

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,10 +1,23 @@
 import Hero from "@/components/ui/sections/Hero";
 import PortfolioContent from "@/components/ui/portfolioContent";
+import About from "@/components/ui/sections/About";
 
 export default async function Home() {
-  return (
-    <PortfolioContent>
-      <Hero />
-    </PortfolioContent>
-  );
+  try {
+    return (
+      <>
+        <PortfolioContent>
+          <Hero />
+          <About />
+        </PortfolioContent>
+      </>
+    );
+  } catch (error) {
+    console.error("Sanity Fetch Error:", error);
+    return (
+      <div className="font-bold text-4xl grid place-content-center">
+        somthing went wrong , please connect to internet{" "}
+      </div>
+    );
+  }
 }
