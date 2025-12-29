00c34b03593d01fd722a027eb37b3201a2baa28c
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-29 17:43:52 +0200

    Refactor StudioPage component to wrap NextStudio in a full-height div and update import path for configuration, improving layout structure.

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { NextStudio } from "next-sanity/studio";
-import config from "../../../sanity.config";
+import config from "@/sanity.config";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return (
+    <div className="h-screen w-full">
+      <NextStudio config={config} />
+    </div>
+  );
 }
