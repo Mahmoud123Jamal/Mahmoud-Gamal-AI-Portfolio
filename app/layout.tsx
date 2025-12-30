79610bb6f2a4eb214cfa1f79a27b4ac7625d093e
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-30 20:52:49 +0200

    Add SanityLive and ChatAi components to RootLayout, enhancing user interaction and real-time content updates in the application layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ChatAi } from "@/components/ui/ChatAi";
+import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function RootLayout({
@@ -8,7 +10,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          {children}
+
+          <SanityLive />
+          <ChatAi />
+        </body>
       </html>
     </ClerkProvider>
   );
