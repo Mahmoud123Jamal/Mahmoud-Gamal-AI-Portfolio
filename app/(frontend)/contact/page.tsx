a2f5942d7883ceca568c7fff5f90cbede335efba
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-01 19:59:29 +0200

    Add Contact page component to display contact section in the frontend layout

---
+++ app/(frontend)/contact/page.tsx
@@ -0,0 +1,11 @@
+import Contact from "@/components/ui/sections/Contact";
+
+function Contactpage() {
+  return (
+    <div className="min-h-screen place-content-center ">
+      <Contact />
+    </div>
+  );
+}
+
+export default Contactpage;
