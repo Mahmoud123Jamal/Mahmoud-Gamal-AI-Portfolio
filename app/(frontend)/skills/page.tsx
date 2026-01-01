c021e2a628ca16812500562898c17b0dee1807e2
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-01 19:58:34 +0200

    Add Skills page component to showcase skills section in the frontend layout

---
+++ app/(frontend)/skills/page.tsx
@@ -0,0 +1,11 @@
+import Skills from "@/components/ui/sections/Skills";
+
+function Skillspage() {
+  return (
+    <div className="min-h-screen mt-25 ">
+      <Skills />
+    </div>
+  );
+}
+
+export default Skillspage;
