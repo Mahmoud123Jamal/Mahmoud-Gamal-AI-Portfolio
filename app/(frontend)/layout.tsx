9731d64f158f07efd78d68ed0ee2b7ed26e71f88
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-01 20:02:26 +0200

    Add ChatAiWrapper to FrontendLayout for enhanced user interaction, integrating Header and Footer components for a complete layout structure.

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,7 +1,18 @@
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import ChaChatAiWrapper from "@/components/ui/ChatAiWrapper";
+
 export default function FrontendLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      {children}
+      <ChaChatAiWrapper />
+      <Footer />
+    </div>
+  );
 }
