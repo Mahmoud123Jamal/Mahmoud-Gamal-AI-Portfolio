cb046247076213a8aff7298c72322b521d0eeb33
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-01 19:59:11 +0200

    Add Projects page component to display project section in the frontend layout

---
+++ app/(frontend)/projects/page.tsx
@@ -0,0 +1,11 @@
+import Projects from "@/components/ui/sections/Projects";
+
+function Projectspage() {
+  return (
+    <div className="mt-6">
+      <Projects />
+    </div>
+  );
+}
+
+export default Projectspage;
