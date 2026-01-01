233e51c1efe2443dae39a6461e8d51f031c48de4
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-01-01 19:58:47 +0200

    Add About page component to provide information about the application in the frontend layout

---
+++ app/(frontend)/about/page.tsx
@@ -0,0 +1,11 @@
+import About from "@/components/ui/sections/About";
+
+function Aboutpage() {
+  return (
+    <div className="min-h-screen mt-16 ">
+      <About />
+    </div>
+  );
+}
+
+export default Aboutpage;
